9318fd0bcb5275e21694fcfb559e5b83a90299b5
mumbimuthiga <veronicmuthiga@gmail.com>
2022-09-02 11:11:54 +0300

    Add the STK PUSH code

---
+++ authorize.php
@@ -0,0 +1,13 @@
+<?
+
+$ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==']);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+echo $response;
